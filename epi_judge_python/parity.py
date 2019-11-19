afdbee50477c909f0ce96520cd7b730a0d494139
Anirudh Deshpande <amd@Anirudhs-MacBook-Pro.local>
2019-11-18 21:34:49 -0800

    Updating program for parity check with more approaches

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,15 +2,49 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
+    """
+    Approach 0, 1 => O(n) runtime
+    """
+    # Approach 0
+    # count = 0
+    #
+    # while x:
+    #     if x & 1 != 0:
+    #         count += 1
+    #     x = x >> 1
 
-    count = 0
-    while x:
-        if x & 1 != 0:
-            count += 1
-        x = x >> 1
+    # return count % 2
 
-    return count % 2
+    # Approach 1
+    # result1 = 0
+    #
+    # while x:
+    #     result1 ^= (x & 1)
+    #     x >>= 1
+    #
+    # return result1
+
+    """
+    Approach 2: O(k) runtime, k = no. of set bits
+    """
+    # Approach 2
+    # result2 = 0
+    #
+    # while x:
+    #     result2 ^= 1
+    #     x = x & (x - 1)
+
+    # return result2
+
+    """
+    Approach 3: O(log n) runtime.
+    """
+    n = 64
+    while n > 1:
+        x ^= x >> int(n/2)
+        n = int(n/2)
+
+    return x & 0x1
 
 
 if __name__ == '__main__':
