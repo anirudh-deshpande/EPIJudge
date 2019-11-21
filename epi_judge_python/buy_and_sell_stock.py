bdf3aaa5452d9a6043e810432f999219b5e07067
Anirudh Deshpande <amd@Anirudhs-MacBook-Pro.local>
2019-11-21 06:41:30 -0800

    Adding program for buy and sell stock

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def buy_and_sell_stock_once(prices):
     # TODO - you fill in here.
-    return 0.0
+    min_so_far = prices[0]
+    max_profit = 0
+
+    for i in range(1, len(prices)):
+        min_so_far = min(min_so_far, prices[i])
+        max_profit = max(max_profit, prices[i]-min_so_far)
+
+    return max_profit
 
 
 if __name__ == '__main__':
